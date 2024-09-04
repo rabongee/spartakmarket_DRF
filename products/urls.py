4e849b333f2af56ebfdf8eef9012330f381367bc
rabongee <rmarkdehdals@gmail.com>
2024-09-05 00:09:16 +0900

    feat: 상품 수정, 삭제 기능 구현
    상품의 정보를 수정하거나 상품을 삭제하는 기능을 구현함
    회원 기능 완성후 로그인과 권한 있는 사용자만 사용할 수 있게 수정 예정

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-app_name = "products"
 urlpatterns = [
-    path("", views.ProductListAPIView.as_view(), name="product_list"),
+    path("", views.ProductListAPIView.as_view()),
+    path("<int:productId>/", views.ProductDetailAPIView.as_view()),
 ]
--- products/views.py
+++ products/views.py
@@ -1,8 +1,9 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
-from .models import Product
-from .serializers import ProductSerializer
 from rest_framework.response import Response
 from rest_framework import status
+from .models import Product
+from .serializers import ProductSerializer
 
 
 class ProductListAPIView(APIView):
@@ -17,3 +18,28 @@ class ProductListAPIView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ProductDetailAPIView(APIView):
+    def get_object(self, productId):
+        return get_object_or_404(Product, id=productId)
+
+    def get(self, request, productId):
+        product = self.get_object(productId)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+
+    def put(self, request, productId):
+        product = self.get_object(productId)
+        serializer = ProductSerializer(
+            product, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, productId):
+        product = self.get_object(productId)
+        product.delete()
+        data = f"{productId}번 게시물 삭제"
+        return Response(data)
