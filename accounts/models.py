eba8a8eaa609986aca30afc3234daf4fcec3ecf0
rabongee <rmarkdehdals@gmail.com>
2024-09-04 20:34:18 +0900

    feat: 회원가입 기능 구현
    간단하게 회원가입 기능을 구현
    비밀먼호 해시화 필요, 이메일 중복검증 기능 필요

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,12 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    nickname = models.CharField(max_length=50)
+    birthday = models.DateField()
+    gender = models.CharField(max_length=10, blank=True, null=True)
+    self_introduction = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.username
