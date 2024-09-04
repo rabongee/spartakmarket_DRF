15f7379dc000d458ed8a2d935344b9b21716516a
rabongee <rmarkdehdals@gmail.com>
2024-09-04 23:15:34 +0900

    feat: 상품 목록 조회, 상품 등록 기능 구현
    상품 목록을 조회하는 기능과 상품 등록 기능을 구현함
    아직 회원기능이 완성되지 않아서 로그인이나 권한 처리등은 하지않음

--- products/urls.py
+++ products/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from . import views
 
+app_name = "products"
 urlpatterns = [
-
+    path("", views.ProductListAPIView.as_view(), name="product_list"),
 ]
--- products/views.py
+++ products/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from .models import Product
+from .serializers import ProductSerializer
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+
+class ProductListAPIView(APIView):
+    def get(self, request):
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
