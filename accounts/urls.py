d5925741c7c8be13babe2dd5344b79a4370ab332
rabongee <rmarkdehdals@gmail.com>
2024-09-05 20:52:02 +0900

    feat: 로그인 기능 구현
    로그인시 JWT를 발행해줌.
    이제 인증기능 구현하면 됨.

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.SingupView.as_view())
+    path('', views.SingupView.as_view()),
+    path('login/', views.LoginView.as_view()),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth import authenticate
 from .models import User
 from .serializers import UserSerializer
 from .validators import validate_signup
@@ -25,3 +27,18 @@ class SingupView(APIView):
         )
         serializer = UserSerializer(user)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class LoginView(APIView):
+    def post(self, request):
+        user = authenticate(username=request.data.get(
+            "username"), password=request.data.get("password"))
+        if not user:
+            return Response({"error": "아이디나 비밀번호가 틀립니다."}, status=status.HTTP_400_BAD_REQUEST)
+        serializer = UserSerializer(user)
+        res_data = serializer.data
+
+        refresh = RefreshToken.for_user(user)
+        res_data['refresh_token'] = str(refresh)
+        res_data['access_token'] = str(refresh.access_token)
+        return Response(res_data)
